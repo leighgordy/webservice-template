2a4dc39846ae6b0d8c4161fc02247ae6d5d61f92
leighgordy <leighgordy@gmail.com>
2020-09-16 11:35:18 +0100

    Added Babel and Koa to application. Added simple hello world app from Koa setup page.

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,11 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  target: 'node',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+  }
 };
