950768d462ff9e943efe4b4083202580b57a926d
leighgordy <leighgordy@gmail.com>
2020-10-18 12:31:40 +0100

    Fixed broken ping test by mocking service-dao

--- test/service/ping.test.js
+++ test/service/ping.test.js
@@ -3,6 +3,18 @@ import Koa from 'koa';
 
 import ping from '../../src/service/ping';
 
+jest.mock('../../src/dao/service-dao', () => (
+  {
+    ...(jest.requireActual('../../src/dao/service-dao')),
+    getServiceConfig: () => Promise.resolve({
+      Item: {
+        version: 1,
+        service: 'webservice-template',
+      },
+    }),
+  }
+));
+
 const app = new Koa();
 
 app.use(ping.routes());
@@ -10,5 +22,10 @@ app.use(ping.routes());
 test('ping test', async () => {
   const response = await request(app.callback()).get('/webservice-template/v1/ping');
   expect(response.status).toBe(200);
-  expect(JSON.parse(response.text)).toStrictEqual({ name: 'webservice-template', status: 'alive' });
+  expect(JSON.parse(response.text)).toStrictEqual({
+    Item: {
+      version: 1,
+      service: 'webservice-template',
+    },
+  });
 });
